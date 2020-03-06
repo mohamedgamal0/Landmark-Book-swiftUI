105ca54b343d0b3ce285b7da4056471dcc1670ba
mohamed gamale <mohamedgamalmohamed48@gmail.comm>
2020-03-06 16:09:47 +0200

    Initial Commit

---
+++ Landmark Book swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmark Book swiftUI
+//
+//  Created by mohamed gamal on 3/6/20.
+//  Copyright © 2020 mohamed gamal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
